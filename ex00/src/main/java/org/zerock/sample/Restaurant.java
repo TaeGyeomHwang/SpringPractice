f50e14d281c0ecba440c02c3e88bd184e6cb6657
DriedPollack <jihuhw99@gmail.com>
2024-03-11 21:44:37 +0900

    Initial commit

---
+++ ex00/src/main/java/org/zerock/sample/Restaurant.java
@@ -0,0 +1,26 @@
+package org.zerock.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.Setter;
+
+@Component
+@Data
+public class Restaurant {
+	//	수정자 주입(롬복)
+	@Setter(onMethod_ = @Autowired)
+	private Chef chef;
+	
+	//	수정자 주입
+//	@Autowired
+//	public void setChef(Chef chef) {
+//		this.chef = chef;
+//	}
+	
+	//생성자 주입(묵시적)
+	public Restaurant(Chef chef) {
+		this.chef = chef;
+	}
+}
