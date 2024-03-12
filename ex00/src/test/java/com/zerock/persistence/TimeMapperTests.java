5f08491b7712d85aa434d5a4a9f0dcb8174e4b49
TaeGyeomHwang <jihuhw99@gmail.com>
2024-03-12 17:50:51 +0900

    20240312

--- ex00/src/test/java/com/zerock/persistence/TimeMapperTests.java
+++ ex00/src/test/java/com/zerock/persistence/TimeMapperTests.java
@@ -1,6 +1,4 @@
-package com.zerock.sample;
-
-import static org.junit.Assert.assertNotNull;
+package com.zerock.persistence;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,22 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.zerock.mapper.TimeMapper;
+
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
-public class SampleTests {
-	@Setter(onMethod_ = {@Autowired})
-	private Restaurant restaurant;
+public class TimeMapperTests {
+	@Setter(onMethod_ = @Autowired)
+	private TimeMapper timeMapper;
 	
 	@Test
-	public void testExist() {
-		assertNotNull(restaurant);
-		
-		log.info(restaurant);
-		log.info("----------------------------");
-		log.info(restaurant.getChef());
+	public void testGetTime() {
+		log.info(timeMapper.getClass().getName());
+		log.info(timeMapper.getTime());
+	}
+	
+	@Test
+	public void testGetTime2() {
+		log.info("getTime2");
+		log.info(timeMapper.getTime2());
 	}
 }
